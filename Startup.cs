e05bc0ba0798a6ebff3ebf53bcfda4d65b2c1298
karim <kf.worknet@gmail.com>
2019-12-24 14:25:01 +0330

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Open the Electron-Window here
+            Task.Run(async () =>
+            {
+                var win = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions() { Title = "Inventory", AutoHideMenuBar = true });
+                win.SetMenuBarVisibility(false);
+            });
+        }
+    }
+}
